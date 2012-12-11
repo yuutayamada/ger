485298470cb1a301835ef4cec1c99e884eafe365
Yuta Yamada <cokesboy@gmail.com>
2012-12-11 21:58:24 +0900

    Add comments

--- lib/ger/command.rb
+++ lib/ger/command.rb
@@ -2,6 +2,9 @@
 #
 # Copyright (C) 2012 Yuta Yamada
 #
+# Author: Yuta Yamada <cokesboy"at"gmail.com>
+#
+# License:
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
